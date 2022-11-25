f0eeb59f715912c12ff495c7e713f4380852f17d
Darya <114287193+dessumt@users.noreply.github.com>
2022-11-25 09:43:55 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -7,9 +7,7 @@ from solar_objects import Star, Planet
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -19,13 +17,17 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
-
+    # print(objects)
     return objects
 
 
@@ -33,35 +35,60 @@ def parse_star_parameters(line, star):
     """Считывает данные о звезде из строки.
     Входная строка должна иметь слеюущий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
     Пример строки:
     Star 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    line = line.split()
+    star.R = float(line[1])
+    star.color = line[2]
+    float_params = []
+    for param in line[3:]:
+        if 'E' in param:
+            idx = param.index('E')
+            float_params.append((float(param[:idx]) * 10**(int(param[idx+1:])))/10**10)
+        else:
+            float_params.append(float(param))
+    star.m = float_params[0]
+    star.x = float_params[1]
+    star.y = float_params[2]
+    star.Vx = float_params[3]
+    star.Vy = float_params[4]
+    # print(float_params)
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
     Предполагается такая строка:
     Входная строка должна иметь слеюущий формат:
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
     Пример строки:
     Planet 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    line = line.split()
+    planet.R = float(line[1])
+    planet.color = line[2]
+    float_params = []
+    for param in line[3:]:
+        if 'E' in param:
+            idx = param.index('E')
+            float_params.append((float(param[:idx]) * 10**(int(param[idx+1:])))/10**10)
+        else:
+            float_params.append(float(param))
+    planet.m = float_params[0]
+    planet.x = float_params[1]
+    planet.y = float_params[2]
+    planet.Vx = float_params[3]
+    planet.Vy = float_params[4]
+    # print(float_params)
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -69,16 +96,15 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Параметры:
-
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            write_string = f"{obj.type} {obj.R} {obj.color} {obj.m} {obj.x} {obj.y} {obj.Vx} {obj.Vy}\n"
+            out_file.write(write_string)
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
